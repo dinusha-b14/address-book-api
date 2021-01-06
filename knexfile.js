ea2ae23ef0cb726896181c97605c162ec8b517e6
dinusha-b14 <dinushab@jobready.com.au>
2021-01-06 14:31:03 +1100

    ensured ssl is used when connecting to heroku postgres

--- knexfile.js
+++ knexfile.js
@@ -47,6 +47,9 @@ module.exports = {
     production: {
         client: 'pg',
         connection: process.env.DATABASE_URL,
+        ssl: {
+            rejectUnauthorized: false
+        },
         migrations: {
             tableName: 'knex_migrations'
         }
